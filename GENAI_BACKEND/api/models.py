15f6682dd304417422f14976ae2d337af288951c
Kai-Hao2002 <daniel20020127@gmail.com>
2025-05-18 21:14:27 +0200

    環境建置

---
+++ GENAI_BACKEND/api/models.py
@@ -0,0 +1,9 @@
+# api/models.py
+
+from django.db import models
+
+class Message(models.Model):
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.content
