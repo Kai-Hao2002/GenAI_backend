2156dd9f9a980e2762e206d8d987ed541850b25a
Kai-Hao2002 <daniel20020127@gmail.com>
2025-05-27 20:22:10 +0200

    accounts後台管理

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+    
+# Setting Djangle Admin Website
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'first_name', 'last_name', 'address', 'is_active', 'is_staff')
+    list_filter = ('is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Data', {'fields': ('first_name', 'last_name', 'email', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_active', 'is_staff')}
+        ),
+    )
+
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if not request.user.is_superuser:
+            return qs.exclude(is_superuser=True)
+        return qs
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
