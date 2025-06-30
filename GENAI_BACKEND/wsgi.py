03cc2406e1a5131e3131762b7ad7db7ee95f3de2
Kai-Hao2002 <daniel20020127@gmail.com>
2025-06-30 20:11:39 +0200

    update

--- GENAI_BACKEND/wsgi.py
+++ GENAI_BACKEND/wsgi.py
@@ -8,12 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GENAI_BACKEND.settings')
 
+# auto migrate
+try:
+    from django.core.management import call_command
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print("Migration failed:", e)
+
 application = get_wsgi_application()
 
-from whitenoise import WhiteNoise
-application = WhiteNoise(application)
